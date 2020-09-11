759b6c05ebf9b3a94873fb5612ac24377485d7a2
snmlm <913813112@qq.com>
2020-09-11 16:13:55 +0800

    Site updated: 2020-09-11 16:13:21

---
+++ js/jsdelivr.js
@@ -0,0 +1,10 @@
+/*if(document.domain != 'localhost'){
+	if(document.getElementById("post")){
+		for (var key in document.getElementById("article-container").getElementsByTagName('img')){
+  			if (isNaN(key)){
+    			break;
+  			}
+    		document.getElementById("article-container").getElementsByTagName('img')[key].src = document.getElementById("article-container").getElementsByTagName('img')[key].src.replace(document.domain,"cdn.jsdelivr.net/gh/snmlm/resources")
+		}
+    }
+}*/
--- js/search/local-search.js
+++ js/search/local-search.js
@@ -54,7 +54,8 @@ $(function () {
 
   function search (path) {
     $.ajax({
-      url: GLOBAL_CONFIG.root + path,
+      //url: GLOBAL_CONFIG.root + path,
+      url: "https://github.com/snmlm/snmlm.github.io/blob/master/search.xml",
       dataType: 'xml',
       success: function (xmlResponse) {
         // get the contents from search data
